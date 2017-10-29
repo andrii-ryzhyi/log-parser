7b2e67225747cca0b156655e959a284d73d4bc31
Andrii Ryzhyi <andrii.nrj@gmail.com>
2017-10-29 22:06:04 +0200

    Add comments

--- parser.py
+++ parser.py
@@ -16,14 +16,14 @@ report = "/home/geny/git/notsobad-testcase/report.log"
 """
 def read_log(log):
 	search_template = openFile(server_log)
-	for line in log:
+	for line in log:  # read server.log
 		line = line.strip()
 		words = line.split()
-		if error_found(words):
+		if error_found(words): # checks for error record
 			trace_id = get_id(words)
 			print "ERROR! Trace id: " + trace_id
-			if trace_id not in error_records:
-				make_report(trace_id, search_template)
+			if trace_id not in error_records: # checks if the same error record hasn't been handled earlier
+				make_report(trace_id, search_template) # record is unique -> forming report
 	search_template.close()
 
 """Checks if current record has ERROR status
@@ -35,27 +35,45 @@ def read_log(log):
 def error_found(words):
 	return True if words[ERR_INDEX] == "ERROR" else False
 
+"""Extracts trace_id from token
+
+    :param line: token, containing trace_id value
+
+    Return trace_id from token
+"""
 def get_id(line):
 	token = line[ERR_INDEX+1].split(",")
 	trace_id = token[1]
 	return trace_id
 
+"""Checks all transactions in server.log file for failed trace_id.
+   If transaction found, records in output report.log file is created grouped by trace_id value.
+
+    :param failed_id: trace_id, extracted from ERROR record
+    :param file: file handler object pointing to server.log. Enables searching all transactions for failed records 
+"""
 def make_report(failed_id, file):
 	error_records.append(failed_id)	
 	customer = ''
 	user = ''
-	for line in file:
+	for line in file:  # start searching for all transactions in server.log for our failed_id case
 	 	line = line.strip()
 	 	words = line.split()
-		curr_id = get_id(words)
+		curr_id = get_id(words) # extracting trace_id for each line
 		if curr_id == failed_id:
 			if words[ERR_INDEX] == "INFO":
-				user = words[-1]
-				customer = get_customer(user)
+				user = words[-1]  # extracting user string from INFO transactions (last token in record)
+				customer = get_customer(user) # getting customer from PostgreSQL by user token
 			target.write('{0:20} {1:38} {2:25} {3:10} {4:12} {5:5} {6:40} {7}\n'.format(curr_id, user, customer, words[0], words[1], words[2], words[7], ' '.join(words[9:])))
 	target.write('-------------------\n')
 	file.seek(0)
 
+"""Forms SQL query to PosgreSQL and extracts customer name for failed records
+
+    :param user: user id extracted from server.log
+
+    Return customer for failed transaction
+"""
 def get_customer(user):
 	query = " ".join((
 				"select c.name from customers c",
@@ -68,6 +86,7 @@ def get_customer(user):
 	customer = row[0]
 	return customer
 
+"""Gracefully handles exception if file does not exist"""
 def openFile(file, modifier="r"):
     try:
       fhandler = open(file, modifier)
@@ -76,6 +95,12 @@ def openFile(file, modifier="r"):
       print "Error: File " + file + " does not exist."
       exit()
 
+"""Forms connection string value for PostgreSQL configuration
+
+    :param yaml_conf: yaml config
+
+    Return connection string based on yaml config values
+"""
 def postgres_connect(yaml_conf):
 	with open(yaml_conf, 'r') as stream:
 		try:
